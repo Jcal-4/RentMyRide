e2aa7b0068050a4c0f86a2c483274f3cbca4dc6f
jairo <jairo_cal@yahoo.com>
2021-03-31 08:20:02 -0700

    api maps working

--- backend/routes/api/cars.js
+++ backend/routes/api/cars.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const asyncHandler = require("express-async-handler");
-const {Car} = require("../../db/models")
+const { Car } = require("../../db/models");
 
 // everything car related api will go here to this file in the backend
 
 // grab all the cars so we are able to render them out anywhere on the page
+router.get(
+	"/car",
+	asyncHandler(async (req, res) => {
+		// do a query for all of the cars in the db
+		const cars = await Car.findAll();
+		console.log(cars);
+		res.send(cars);
+	})
+);
 
-router.get("/car", asyncHandler( async (req, res) => {
-  // do a query for all of the cars in the db
-  const cars = await Car.findAll();
-  console.log(cars)
-  res.send(cars)
-}))
+// grab a car by id. This will be helpful for when grabbing a car by id from the url but this might not be needed due to the one above
+// if we have already grabbed all of the cars then there might not be a need to get a specific car from the id. We might be able to just grab it
+// from the state and by a key #!
+
+// router.get("/car", asyncHandler( async (req, res) => {
+//   // do a query for all of the cars in the db
+//   const cars = await Car.findAll();
+//   console.log(cars)
+//   res.send(cars)
+// }))
 
 // is the router being used correctly in another file (imported and used)?
-module.exports = router
+module.exports = router;
