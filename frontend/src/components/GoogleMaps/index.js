cc59c6a0d48e6351f0720fe0ece7782ffb201f5e
jairo <jairo_cal@yahoo.com>
2021-04-04 14:26:25 -0700

    markers added

--- frontend/src/components/GoogleMaps/index.js
+++ frontend/src/components/GoogleMaps/index.js
@@ -39,16 +39,25 @@ const mapContainerStyle = {
 };
 // the center is where the map will first load into (COME BACK AND EDIT THIS TO THE USERS SAVED ADDRESS)
 const center = {
-	lat: 36.169941,
-	lng: -115.139832,
+	lat: 40.73061,
+	lng: -74.006,
 };
 
 // GOOGLE MAP COMPONENT (main render) --------------------------------------------------------------------------------
 export default function GoogleMaps() {
 	let cars = useSelector((state) => state.car.carsList);
 	Geocode.setApiKey("AIzaSyC_6E8DK05Pld0jbbPYVvX1SIATom7GR6Q");
-	let addressesGeocode = []; // will store all the lat/lng in this array
-
+	// let addressesGeocode = []; // will store all the lat/lng in this array
+	let addressesGeocode = [
+		{ lat: 36.18794, lng: -115.05236 },
+		{ lat: 36.12162, lng: -115.30318 },
+		{ lat: 36.22216, lng: -115.28787 },
+		{ lat: 36.22349, lng: -115.16928 },
+		{ lat: 36.2036749, lng: -115.1526188 },
+		{ lat: 33.9602649, lng: -118.3877674 },
+		{ lat: 32.66367, lng: -116.27363 },
+		{ lat: 34.049209, lng: -118.466374 },
+	];
 	// function geocodee(cars) {
 	// 	cars.forEach((car) => {
 	// 		Geocode.fromAddress(car.address).then(
@@ -109,7 +118,14 @@ export default function GoogleMaps() {
 				options={options}
 				center={center}
 				onLoad={onMapLoad}
-			></GoogleMap>
+			>
+				{addressesGeocode.map((marker) => (
+					<Marker
+						key={`${marker.lat}-${marker.lng}`}
+						position={{ lat: marker.lat, lng: marker.lng }}
+					/>
+				))}
+			</GoogleMap>
 		</div>
 	);
 }
